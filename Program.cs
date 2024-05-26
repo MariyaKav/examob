4a4608a8afd245f1639e9948f0bb0656cab5302f
Mariya Kaverina <kaverina.ma@mail.ru>
2024-05-26 23:13:38 +0400

    Added solution to task

--- Program.cs
+++ Program.cs
@@ -18,3 +18,46 @@ int CountItems(string[] arr, int max)
     }
     return count;                                       // возвращаем результат
 }
+// создаём новый массив и заполняем его элементами, длина которых меньше заданной
+string[] NewArray(string[] arr, int max)
+{
+    int newArrayLenght = CountItems(arr, max);          // узнаем, сколько элементов массива меньше заданной длины
+    string[] newArray = new string[newArrayLenght];     // создаем новый массив полученной длины
+    int newArrayIndex = 0;                              // счетчик индексов нового массива
+    for (int i = 0; i < arr.Length; i++)                // перебираем исходный массив
+    {
+        if (arr[i].Length <= max)                       // проверяем, подходит ли элемент массива заданным условиям
+        {
+            newArray[newArrayIndex] = arr[i];           // копируем элемент в новый массив
+            newArrayIndex++;                            // увеличиваем индекс
+        }
+    }
+    return newArray;                                    // возвращаем новый массив
+}
+
+// выводим массив в консоль в красивом формате
+void PrintArray(string[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write('"');
+        System.Console.Write(array[i]);
+        System.Console.Write('"');
+        if (i < array.Length - 1) System.Console.Write(", ");
+    }
+    System.Console.Write("]");
+}
+
+// выводим итоговый результат
+Console.Write("Введите набор символов через знак (,) : ");
+string inStringSimbol = Console.ReadLine();
+string[] arraySimbol = GetArrayStringConsole(inStringSimbol);
+Console.WriteLine();
+Console.WriteLine("Введенные символы : ");
+PrintArray(arraySimbol);
+Console.WriteLine();
+Console.WriteLine("Итоговый массив со строками длиной меньше " + max +":");
+string[] newArray = NewArray(arraySimbol, max);
+
+PrintArray(newArray);
